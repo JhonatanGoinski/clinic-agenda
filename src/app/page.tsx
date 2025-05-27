dc43b885df75ed470dfc4f0f2a767bc9f9a492d6
Jhonatan Miguel Goinski <158315136+JhonatanGoinski@users.noreply.github.com>
2025-05-27 00:26:27 -0300

    Configuração shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center">
-      <h1 className="flex gap-3">Home ia</h1>
-    </div>
-  );
+  return <Button>Click me</Button>;
 }
