c114f885aa86142d058d8061b60f8b224fa3b798
Jhonatan Miguel Goinski <158315136+JhonatanGoinski@users.noreply.github.com>
2025-05-27 23:35:10 -0300

    feat: bloqueando paginação em estado quando está deslogado ou logado

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,10 +1,18 @@
-"use client";
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LoginForm from "./components/login-form";
 import SignUpForm from "./components/sign-up-form";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 
-const AuthenticationPage = () => {
+const AuthenticationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session?.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex h-screen items-center justify-center p-2">
       <Tabs defaultValue="login" className="w-[400px]">
