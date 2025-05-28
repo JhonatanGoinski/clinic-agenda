3f1c03072ddb640f063a87fdc3d00484d85de977
Jhonatan Miguel Goinski <158315136+JhonatanGoinski@users.noreply.github.com>
2025-05-28 00:30:46 -0300

    feat: adicionando pagina do formulario

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import ClinicForm from "./components/form";
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open={true}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicione uma clínica</DialogTitle>
+            <DialogDescription>
+              Preencha o formulário abaixo para adicionar uma clínica.
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
